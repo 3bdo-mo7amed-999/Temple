ea323bdba14b8a674ad71053f7ffb3b06f3faaa8
3bdo-mo7amed-999 <abdo.mohamed.abo.abdo@gmail.com>
2024-12-03 21:38:01 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./components/assets/style.css"
 import { useRoutes } from "react-router-dom";
-import Home from "./home/Home";
-import Shop from "./shop/Shop";
-import About from "./about/About";
-import Contact from "./contact/Contact";
+import Home from "./Home/Home";
+import Shop from "./Shop/Shop";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
 import NotFound from "./NotFound/NotFound";
 function App() {
   const routes = useRoutes([
